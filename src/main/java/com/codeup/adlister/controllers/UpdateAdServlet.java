f34269ab3d7ed073bd49b61fa75063a8f918a7a4
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-01 12:45:40 -0500

    update

--- src/main/java/com/codeup/adlister/controllers/UpdateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/UpdateAdServlet.java
@@ -1,47 +1,34 @@
 package com.codeup.adlister.controllers;
-
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 @WebServlet(name = "controllers.EditAdServlet", urlPatterns = "/ads/update")
 public class UpdateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
         }
-        if (request.getSession().getAttribute("ad") != null) {
-            Ad adEdit = (Ad) request.getSession().getAttribute("ad");
-        }
         request.getRequestDispatcher("WEB-INF/ads/update.jsp").forward(request, response);
     }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        long ad_id = Long.parseLong(request.getParameter("ad_id"));
-        Ad adToEdit = DaoFactory.getAdsDao().getAdByID(ad_id);
-        String title = request.getParameter("title");
-        String description = adToEdit.getDescription();
-
+        long ad_id_update = Long.parseLong(request.getParameter("id"));
+        String title_update = request.getParameter("title");
+        String description_update = request.getParameter("description");
         //validate input
-        boolean inputHasErrors = title.isEmpty() || description.isEmpty();
-
+        boolean inputHasErrors = title_update.isEmpty() || description_update.isEmpty();
         if (inputHasErrors) {
-            request.setAttribute("error", "Error handling your request");
+            request.setAttribute("error", "title or description can't be empty");
             request.getRequestDispatcher("WEB-INF/ads/update.jsp").forward(request, response);
         }
-
         Ad sessionAd = (Ad) request.getSession().getAttribute("ad");
         //create and save a new ad
-        Ad ad_edit = new Ad(title, description);
-        ad_edit
-
-
+        Ad ad_update = new Ad(sessionAd.getUserId(), title_update, description_update);
+        ad_update.setId(sessionAd.getId());
     }
 }
